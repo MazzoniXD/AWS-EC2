6ae9f6d17bdd5af3531470278988529d75047e27
MazzoniXD <thiagodiasmazzoni41@gmail.com>
2022-03-21 17:09:27 -0300

    Novamente

--- variables.tf
+++ variables.tf
@@ -3,11 +3,6 @@ variable "region" {
   default     = "us-east-1"
 }
 
-variable "profile" {
-  description = "Profile of the application"
-  default = "tf069"
-}
-
 variable "name" {
   description = "Name of the application"
   default     = "ThiagoMachine"
